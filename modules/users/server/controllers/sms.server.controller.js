4c9ede2b0f927e0d6d67763baca4bdfa98f69ed6
Santiago <santidltp@hotmail.com>
2016-12-27 14:51:47 -0500

    added controller for sms

---
+++ modules/users/server/controllers/sms.server.controller.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var path = require('path'),
+  mongoose = require('mongoose'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  _ = require('lodash');
+
+/**
+ * Create a Sm
+ */
+exports.create = function (req, res) {
+
+};
+
+/**
+ * Show the current Sm
+ */
+exports.read = function (req, res) {
+
+};
+
+/**
+ * Update a Sm
+ */
+exports.update = function (req, res) {
+
+};
+
+/**
+ * Delete an Sm
+ */
+exports.delete = function (req, res) {
+
+};
+
+/**
+ * List of Sms
+ */
+exports.list = function (req, res) {
+
+};
