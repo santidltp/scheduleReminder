567db9a0ff8f082806145794061dc9a62aa2600a
Santiago <santidltp@hotmail.com>
2016-12-28 18:54:28 -0500

    getting reminders from DB in angular

--- modules/users/client/controllers/remindertiles/reminder.client.controller.js
+++ modules/users/client/controllers/remindertiles/reminder.client.controller.js
@@ -3,6 +3,11 @@
 angular.module('users').controller('RemindertilesController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
     function ($scope, $stateParams, $http, $location, Authentication) {
         $scope.authentication = Authentication;
-        
+        $http.get('/api/users/reminders').success(function (data) {
+            $scope.reminders = data;
+        }).
+        error(function (err) {
+            console.log("this error was encounter: " + err)
+        });
     }
 ]);
