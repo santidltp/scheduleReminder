6d74eb84793c2fc549783c10106eaade461f7a9b
Santiago <santidltp@hotmail.com>
2016-12-28 16:51:17 -0500

    added conditional to see if user is logged in and fixed formatting

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,8 +4,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-    console.log(Authentication.user);
-    if (Authentication.user ==  "") {
+    if (Authentication.user == "") {
       $scope.authenticated = true;
     } else {
       $scope.authenticated = false;
--- modules/users/client/controllers/remindertiles/reminder.client.controller.js
+++ modules/users/client/controllers/remindertiles/reminder.client.controller.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('users').controller('PasswordController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
-  function ($scope, $stateParams, $http, $location, Authentication) {
-    $scope.authentication = Authentication;
-  }
+    function ($scope, $stateParams, $http, $location, Authentication) {
+        $scope.authentication = Authentication;
+        if (Authentication.user == "") {
+            $scope.authenticated = true;
+        } else {
+            $scope.authenticated = false;
+        }
+    }
 ]);
