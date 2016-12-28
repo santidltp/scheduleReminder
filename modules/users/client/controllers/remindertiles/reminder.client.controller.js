93e04aa4021657b24ae959cd24090086076082fb
Santiago <santidltp@hotmail.com>
2016-12-28 17:22:33 -0500

    fixed unnecessary typo

--- modules/users/client/controllers/remindertiles/reminder.client.controller.js
+++ modules/users/client/controllers/remindertiles/reminder.client.controller.js
@@ -3,10 +3,6 @@
 angular.module('users').controller('RemindertilesController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
     function ($scope, $stateParams, $http, $location, Authentication) {
         $scope.authentication = Authentication;
-        if (Authentication.user == "") {
-            $scope.authenticated = true;
-        } else {
-            $scope.authenticated = false;
-        }
+        
     }
 ]);
