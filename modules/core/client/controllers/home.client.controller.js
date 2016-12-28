267282e782e4c6e771eff45432445adaa373db4d
Santiago <santidltp@hotmail.com>
2016-12-28 17:22:01 -0500

    deleted unnecessary conditional and fixed typo

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,10 +4,5 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-    if (Authentication.user == "") {
-      $scope.authenticated = true;
-    } else {
-      $scope.authenticated = false;
-    }
   }
 ]);
