ee74466997f3eccea0091e4c0713407b1a48cfd8
Santiago <santidltp@hotmail.com>
2016-12-28 16:00:34 -0500

    modiefied controller for the ui for loging in

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,5 +4,11 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    console.log(Authentication.user);
+    if (Authentication.user ==  "") {
+      $scope.authenticated = true;
+    } else {
+      $scope.authenticated = false;
+    }
   }
 ]);
