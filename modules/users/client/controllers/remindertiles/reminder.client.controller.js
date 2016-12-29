d5111d545b452cf8860d5b6f53062129988a4cf5
Santiago <santidltp@hotmail.com>
2016-12-29 13:52:34 -0500

    added a modal with angularjs

--- modules/users/client/controllers/remindertiles/reminder.client.controller.js
+++ modules/users/client/controllers/remindertiles/reminder.client.controller.js
@@ -1,15 +1,41 @@
 'use strict';
 
-angular.module('users').controller('RemindertilesController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
-    function ($scope, $stateParams, $http, $location, Authentication) {
+angular.module('users').controller('RemindertilesController', ['$scope', '$stateParams', '$http', '$location', 'Authentication', '$modal',
+    function ($scope, $stateParams, $http, $location, Authentication, $modal) {
         $scope.authentication = Authentication;
         //Get active reminders
-        $http.get('/api/users/reminders').success(function (data) {
-            $scope.reminders = data;
-        }).
-        error(function (err) {
-            console.log("this error was encounter: " + err)
-        });
+        // $http.get('/api/users/reminders').success(function (data) {
+        //     $scope.reminders = data;
+        // }).
+        // error(function (err) {
+        //     console.log("this error was encounter: " + err)
+        // });
+        $scope.items = ['item1', 'item2', 'item3'];
+        $scope.open = function () {
+            var myModal = $modal.open({
+                templateUrl: 'modules/users/views/remindertiles/newsms.html',
+                size: 'sm',
+                controller: function ($scope, $modalInstance, items) {
+                    
+                    $scope.items = items;
+                    $scope.selected = {
+                        item: $scope.items[0]
+                    };
+
+                    $scope.ok = function () {
+                        $modalInstance.close($scope.selected.item);
+                    };
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+        }
     }
-    
 ]);
