04c61d9bfd3e990d5a377d94f32e31e4b290c7f5
Santiago <santidltp@hotmail.com>
2016-12-28 20:03:31 -0500

    added comment

--- modules/users/client/controllers/remindertiles/reminder.client.controller.js
+++ modules/users/client/controllers/remindertiles/reminder.client.controller.js
@@ -3,6 +3,7 @@
 angular.module('users').controller('RemindertilesController', ['$scope', '$stateParams', '$http', '$location', 'Authentication',
     function ($scope, $stateParams, $http, $location, Authentication) {
         $scope.authentication = Authentication;
+        //Get active reminders
         $http.get('/api/users/reminders').success(function (data) {
             $scope.reminders = data;
         }).
