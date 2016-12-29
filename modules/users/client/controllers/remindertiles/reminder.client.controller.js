d5c9893b7caf82247cd70d3f2bebb4a9ac94f487
Santiago <santidltp@hotmail.com>
2016-12-29 14:07:02 -0500

    uncommnted modal tiles

--- modules/users/client/controllers/remindertiles/reminder.client.controller.js
+++ modules/users/client/controllers/remindertiles/reminder.client.controller.js
@@ -3,20 +3,20 @@
 angular.module('users').controller('RemindertilesController', ['$scope', '$stateParams', '$http', '$location', 'Authentication', '$modal',
     function ($scope, $stateParams, $http, $location, Authentication, $modal) {
         $scope.authentication = Authentication;
-        //Get active reminders
-        // $http.get('/api/users/reminders').success(function (data) {
-        //     $scope.reminders = data;
-        // }).
-        // error(function (err) {
-        //     console.log("this error was encounter: " + err)
-        // });
+        // Get active reminders
+        $http.get('/api/users/reminders').success(function (data) {
+            $scope.reminders = data;
+        }).
+        error(function (err) {
+            console.log("this error was encounter: " + err)
+        });
         $scope.items = ['item1', 'item2', 'item3'];
         $scope.open = function () {
             var myModal = $modal.open({
                 templateUrl: 'modules/users/views/remindertiles/newsms.html',
                 size: 'sm',
                 controller: function ($scope, $modalInstance, items) {
-                    
+
                     $scope.items = items;
                     $scope.selected = {
                         item: $scope.items[0]
