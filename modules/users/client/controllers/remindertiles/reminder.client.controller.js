6ba48ad6e3c115741ffa297527adb5faa267b0aa
Santiago <santidltp@hotmail.com>
2016-12-30 16:19:55 -0500

    modal sends data to db

--- modules/users/client/controllers/remindertiles/reminder.client.controller.js
+++ modules/users/client/controllers/remindertiles/reminder.client.controller.js
@@ -3,6 +3,8 @@
 angular.module('users').controller('RemindertilesController', ['$scope', '$stateParams', '$http', '$location', 'Authentication', '$modal',
     function ($scope, $stateParams, $http, $location, Authentication, $modal) {
         $scope.authentication = Authentication;
+         $scope.reminders =[];
+        
         // Get active reminders
         $http.get('/api/users/reminders').success(function (data) {
             $scope.reminders = data;
@@ -10,9 +12,46 @@ angular.module('users').controller('RemindertilesController', ['$scope', '$state
         error(function (err) {
             console.log("this error was encounter: " + err)
         });
+        $scope.phonenumber = "";
+        $scope.when = "";
+        $scope.message = "";
 
 
-        $scope.create =function(){
+        $scope.send = function myFunction() {
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+            var body = {
+                phoneNumber: this.phonenumber,
+                when: this.when,
+                message: this.message
+            };
+            setTimeout(function () {
+                alert("sent!!");
+                $http({
+                    url: '/api/users/reminders',
+                    method: "POST",
+                    data: body,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }).success(function (response) {
+                    $scope.reminders.push(response.reminders);
+                    console.log($scope.reminders);
+                }).error(function (response) {
+                    $scope.error = response.message;
+                });
+
+            }, 3000);
+
+        };
+        $scope.create = function () {
+            $scope.phonenumber = this.phonenumber;
+            $scope.when = this.when;
+            $scope.message = this.message;
+            this.send();
             alert(this.when);
         };
         $scope.open = function () {
@@ -39,7 +78,7 @@ angular.module('users').controller('RemindertilesController', ['$scope', '$state
 
             });
             modalInstance.result.then(function (selectedItem) {
-                console.log(selectedItem);
+                // console.log(selectedItem);
             }, function () {
                 $log.info('Modal dismissed at: ' + new Date());
             });
